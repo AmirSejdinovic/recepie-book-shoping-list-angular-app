1a31449f3c94b8c5bd967f655a99b6d0a9217da7
Amir Sejdinovic <amir24V@yahoo.com>
2019-11-22 13:50:16 +0100

    Building Components structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
+import { RecipesDetailComponent } from './recipes/recipes-detail/recipes-detail.component';
+import { RecipesItemComponent } from './recipes/recipes-list/recipes-item/recipes-item.component';
+import { ShopingListComponent } from './shoping-list/shoping-list.component';
+import { ShoppingEditComponent } from './shoping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipesListComponent,
+    RecipesDetailComponent,
+    RecipesItemComponent,
+    ShopingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule

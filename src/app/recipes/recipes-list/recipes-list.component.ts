39e78680322693a2c558fdb50e4a73dd3b58cb47
Amir Sejdinovic <amir24V@yahoo.com>
2019-11-26 13:29:16 +0100

    Importing model

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+//importung model
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+//creating the recipes variable with calling the class from my model after that i manualy call the method and passed it arguments
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'This is the test', 'https://joyfoodsunshine.com/wp-content/uploads/2016/09/easy-pizza-casserole-recipe-4-500x500.jpg'),
+  ];
 
   constructor() { }
 

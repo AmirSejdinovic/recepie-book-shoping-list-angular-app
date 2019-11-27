d38e5d9fb8563a8e0460477e57298d9c484fdada
Amir Sejdinovic <amir24V@yahoo.com>
2019-11-27 13:04:53 +0100

    Importing the module class in the component

--- src/app/shoping-list/shoping-list.component.ts
+++ src/app/shoping-list/shoping-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+//Importing the class from the module we created
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shoping-list',
@@ -6,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shoping-list.component.css']
 })
 export class ShopingListComponent implements OnInit {
-  ingredients = [];
+  //Creating ingredients propraty in the this class and adding it the typ of the model name (the model we created and stored in the shared folder, and here we pass the name of that class).
+  ingredients: Ingredient[]  = [
+    //Calling the class of model and passig the parametars
+    new Ingredient('Appels', 5),
+    new Ingredient('Tomatoes', 10)
+  ];
 
   constructor() { }
 

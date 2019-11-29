015451e49287fd25eb3152130e6093b75e5fe428
Amir Sejdinovic <amir24V@yahoo.com>
2019-11-29 10:28:07 +0100

    Creating the funcionality that display only the component when the condicion from ngIf is meet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+  //Creating the proprety of the loadedFeature with asignin the starter value.
+  loadedFeature = 'recipe';
+  //Creating the method which I called on the template file and passing the reciving arguments and updating the proprety from top
+  onNavigate(feature: string){
+     this.loadedFeature = feature;
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+//Importing the oputput directive and EvetEmitter class from angualr core
+import { Component, Output, EventEmitter } from '@angular/core';
+import { EventEmitter } from 'events';
 
 
 @Component({
@@ -8,5 +10,11 @@ import { Component } from '@angular/core';
 
 })
 export class HeaderComponent{
+  //Creating the proprety wihic will emit the data from arguments inisde the method. I use here the Angular core class EventEmitter to emit data, and I also use the Angular directive @output which will output data from this proprety and I can catch that data from another component
+  @Output() featureSelected = new EventEmitter<string>();
+//Here I created the method which I call in the template file on click. There i pased the arguments for links and here i store that argumants in the parametars feature and updating the top proprety with that data and emit the value
+  onSelect(feature: string){
+     this.featureSelected.emit(feature);
+  }
 
 }

f5611a344556f007bc86168faa3cb288322d6140
Amir Sejdinovic <amir24V@yahoo.com>
2019-12-02 10:24:50 +0100

    Creating the funciontalty for adding ingredients

--- src/app/shoping-list/shoping-list.component.ts
+++ src/app/shoping-list/shoping-list.component.ts
@@ -18,6 +18,11 @@ export class ShopingListComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  } 
+  //Creating the method
+  onIngredientAdded(ingredient: Ingredient){
+    this.ingredients.push(ingredient);
+
   }
 
 }
--- src/app/shoping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shoping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, EventEmitter, Output } from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
+
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  //Creating the propreties
+   @ViewChild('nameInput', {static: false}) nameInputRef: ElementRef;
+   @ViewChild('amountInput', {static:false}) amountInputRef: ElementRef;
+   @Output() ingredientAdded = new EventEmitter<{Ingredient}>();
 
   constructor() { }
 
   ngOnInit() {
+  }
+  //Creating the method
+  onAddItem(){
+    const ingName = this.nameInputRef.nativeElement.value;
+    const ingAmoun = this.amountInputRef.nativeElement.value;
+     const newIngredient = new Ingredient(ingName, ingAmoun);
+     this.ingredientAdded.emit(newIngredient);
   }
 
 }
